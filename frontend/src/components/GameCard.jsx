7cca46f4625d5274e27e93470e8eb6fba548d765
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-07-22 13:26:43 +0530

    Update GameCard.jsx

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,13 +1,24 @@
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import { useAuth } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './GameCard.css'
 
 export default function GameCard({name, image}){
+    const {isLoggedIn} = useAuth();
+    const navigate = useNavigate();
+
+    const playGame = ()=>{
+        if(!isLoggedIn){
+            navigate('/signup');
+        }
+    }
+
     return(
         <div className="game-card">
             <img src={image}/>
             <div className="game-info">
                 <h1>{name}</h1>
-                <PlayCircleIcon className='play'/>
+                <PlayCircleIcon className='play' onClick={playGame}/>
             </div>
         </div>
     )
